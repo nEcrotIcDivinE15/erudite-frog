e016a708ed9271072b457cdcaa794e4615fc5751
Dom <dominicgesling1234@gmail.com>
2020-09-23 10:41:42 -0700

    implement jquery

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-console.log("Fuck you");
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	apiKey: "AIzaSyDRgQ2FOfteuiDNld3PlzFaZVnwGpWPmwI",
+	authDomain: "erudtie-frog.firebaseapp.com",
+	databaseURL: "https://erudtie-frog.firebaseio.com",
+	projectId: "erudtie-frog",
+	storageBucket: "erudtie-frog.appspot.com",
+	messagingSenderId: "60178020365",
+	appId: "1:60178020365:web:58b477b6591eb6fde43d44",
+	measurementId: "G-GGP7VQ53QL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+console.log()
